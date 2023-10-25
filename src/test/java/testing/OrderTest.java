30618e4217f50a95309b44c67e56426c23b1539d
Nique1 <dominikasarata@gmail.com>
2023-10-25 14:39:12 +0200

    Using hamcrest in testing List

---
+++ src/test/java/testing/OrderTest.java
@@ -0,0 +1,93 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class OrderTest {
+
+    @Test
+    void testAssertArrayEquals() {
+        //given
+        int[] ints1 = {1, 2, 3};
+        int[] ints2 = {1, 2, 3};
+        //when
+        //then
+        assertArrayEquals(ints1,ints2);
+    }
+
+    @Test
+    void mealListShouldBeEmptyAfterCreationOfOrder(){
+        //given
+        Order order = new Order();
+        //when
+        //then
+        assertThat(order.getMeals(),empty());
+        assertThat(order.getMeals().size(), equalTo(0));
+        assertThat(order.getMeals(), hasSize(0));
+        assertThat(order.getMeals(), emptyCollectionOf(Meal.class));
+
+    }
+
+    @Test
+    void addingMealToOrderShouldIncreaseOrderSize(){
+        //given
+        Meal meal = new Meal(20, "Pizza");
+        Meal meal2 = new Meal(25, "Pasta");
+        Order order = new Order();
+        //when
+        order.addMealToOrder(meal);
+        //then
+        assertThat(order.getMeals(),hasSize(1));
+        assertThat(order.getMeals(), contains(meal));
+        assertThat(order.getMeals(), hasItem(meal));
+    }
+
+    @Test
+    void removingMealFromOrderShouldDecreaseOrderSize(){
+        //given
+        Meal meal = new Meal(20, "Pizza");
+        Order order = new Order();
+        //when
+        order.addMealToOrder(meal);
+        order.removeMealFromOrder(meal);
+        //then
+        assertThat(order.getMeals(), hasSize(0));
+        assertThat(order.getMeals(), not(contains(meal)));
+    }
+
+    @Test
+    void mealsShouldBeInCorrectOrderAfterAddingThenToOrder(){
+        //given
+        Meal meal1 = new Meal(20, "Pizza");
+        Meal meal2 = new Meal(25, "Pasta");
+        Order order = new Order();
+        //when
+        order.addMealToOrder(meal1);
+        order.addMealToOrder(meal2);
+        //then
+        assertThat(order.getMeals(), contains(meal1,meal2));
+        assertThat(order.getMeals(), containsInAnyOrder(meal1,meal2));
+    }
+
+    @Test
+    void testIfTwoMealListAreTheSame(){
+        //given
+        Meal meal1 = new Meal(20, "Pizza");
+        Meal meal2 = new Meal(25, "Pasta");
+        Meal meal3 = new Meal(10, "Sandwich");
+        Order order = new Order();
+        //when
+        List<Meal> meals1 = Arrays.asList(meal1,meal2,meal3);
+        List<Meal> meals2 = Arrays.asList(meal1,meal2,meal3);
+
+        //then
+        assertThat(meals1, is(meals2));
+    }
+
+}
