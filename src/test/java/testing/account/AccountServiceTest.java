83ca5504cd17e0c8dd154765bb6af28da7d01ba0
Nique1 <dominikasarata@gmail.com>
2023-11-17 12:58:19 +0100

    Mock implementation

--- src/test/java/testing/account/AccountServiceTest.java
+++ src/test/java/testing/account/AccountServiceTest.java
@@ -6,18 +6,35 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-class AccountServiceTest {
+public class AccountServiceTest {
     @Test
     void getAllActiveAccounts(){
         //given
-        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
-        AccountService accountService = new AccountService(accountRepositoryStub);
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        when(accountRepository.getAllAccounts()).thenReturn(accounts);
         //when
         List<Account> accountList = accountService.getAllActiveAccounts();
         //then
         assertThat(accountList, hasSize(2));
     }
+    private List<Account> prepareAccountData(){
+        Address address1 = new Address("Kocykowa", "21/3");
+        Account account1 = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address3 = new Address("Limanowska", "31");
+        Account account3 = new Account(address3);
+
+        List<Account> listOfAccounts = List.of(account1,account2, account3);
+
+        return listOfAccounts;
+    }
+
 
 }
